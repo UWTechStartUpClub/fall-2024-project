3b017c089dd8df7a562457a440a82ff99bb55d7f
Jacob Klymenko <klymenkojacob@gmail.com>
2024-11-07 19:53:37 -0800

    feat: add register form with axios

--- stock-app/src/App.jsx
+++ stock-app/src/App.jsx
@@ -1,25 +1,29 @@
 
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
+import Register from './components/Forms/Register';
+import Login from './components/Forms/Login';
 import Stock from './routes/Stock';
 
 function App() {
   return (
-   <>
-   <Routes>
-    <Route path="/" element={<Home />} />
-    {/* <Route path="/about" element={<About />} />
-    <Route path="/t&c" element={<TermsAndConds />} />
-    <Route path="/contact" element={<Contact />} />
-    <Route path="/register" element={<Register />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/profile/:userId" element={<UserProfile />} />
-    <Route path="/stock/" element={<AllStocks />} />
-    <Route path="/stock/:symbol/graph" element={<StockGraph />} />
-    <Route path="/stock/:symbol" element={<Stock />} />
-    <Route path="/search" element={<Search />} /> */}
-   </Routes>
-   </>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+        {/* <Route path="/about" element={<About />} />
+        <Route path="/t&c" element={<TermsAndConds />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile/:userId" element={<UserProfile />} />
+        <Route path="/stock/" element={<AllStocks />} />
+        <Route path="/stock/:symbol/graph" element={<StockGraph />} />
+        <Route path="/stock/:symbol" element={<Stock />} />
+        <Route path="/search" element={<Search />} /> */}
+      </Routes>
+    </>
   );
 }
 
