c34bce49dfc2c335a71f671fb8b8531118f4963f
Jacob Klymenko <klymenkojacob@gmail.com>
2024-11-18 18:14:46 -0800

    style: minor styling adjustments

--- stock-app/src/App.jsx
+++ stock-app/src/App.jsx
@@ -1,4 +1,3 @@
-
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import About from './routes/About';
@@ -14,23 +13,19 @@ import Search from './routes/Search';
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="register" element={<Register />} /> // not sure if needed
-        <Route path="login" element={<Login />} /> */}
-        <Route path="/about" element={<About />} />
-        <Route path="/t&c" element={<TermsAndConds />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile/:userId" element={<UserProfile />} />
-        <Route path="/stock/" element={<AllStocks />} />
-        <Route path="/stock/:symbol/graph" element={<StockGraph />} />
-        <Route path="/stock/:symbol" element={<Stock />} />
-        <Route path="/search" element={<Search />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/t&c" element={<TermsAndConds />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/profile/:userId" element={<UserProfile />} />
+      <Route path="/stock/" element={<AllStocks />} />
+      <Route path="/stock/:symbol/graph" element={<StockGraph />} />
+      <Route path="/stock/:symbol" element={<Stock />} />
+      <Route path="/search" element={<Search />} />
+    </Routes>
   );
 }
 
