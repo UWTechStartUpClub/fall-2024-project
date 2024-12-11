aff4aa6e5d02f05f403ea3b95173e8e763fbdabe
presiauw0 <psia97@uw.edu>
2024-12-10 23:15:05 -0800

    feature: watchlist which loads data (in theory, haven't test live), can add and delete entries.

--- stock-app/src/components/Watchlist/index.jsx
+++ stock-app/src/components/Watchlist/index.jsx
@@ -5,52 +5,121 @@ import style from './index.module.css';
 
 const Watchlist = () => {
     const [stockList, setStockList] = useState([]);
-    const [showAddMenu, setShowAddMenu] = useState(false);
+    const [error, setError] = useState([]); // Hold error info for each stock request
+    const [showAddMenu, setShowAddMenu] = useState(false); // hold state determining whether to show the "Add stock" menu
 
-    const handleAddToList = (theSymbol, theData) => {
-        // Add to shared hook
+    // TEMPORARY LIST OF STOCKS
+    const stockWatch = ["IBM", "NVDA", "AAPL"];
+    const stockWatch2 = ["IBM", "IBM", "IBM"];
 
-        if (!(theSymbol === undefined || theData === undefined)) {
-            setStockList([
-                ...stockList,
-                {"symbol": theSymbol, "data": theData}
-            ]);
-        }
-    }
+    const getRandomNum = () => {
+        const min = 1000;
+        const max = 10000;
 
-    const getStockDataTemp = (sym) => {
+        /*
+        https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+        */
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+
+    /*
+    const fetchStockData = async (symb) => {
+        // random number generation for ID
+        const randomNum = getRandomNum();
+        const theId = symb + randomNum;
+
         try {
-            const response = TemporaryServ(sym);
-            return response;
-        } catch(err) {
-            console.error("An error occured: " + err);
-        }
-    }
+            console.log("Fetching data...");
+            const response = await axios.get(`stock/${symb}`);
+            // functional update
+            setStockList(prevState => {
+                let objInsert = [...prevState,
+                    {
+                        "id": theId,
+                        "symbol": symb,
+                        "data": response["Global Quote"]
+                    }
+                ];
 
-
-    // ***Load user's existing list data***
-    const buildListFromArray = (theArray) => {
-        const data = [];
-
-        for (let i = 0; i < theArray.length; i++) {
-            const quote = getStockDataTemp(theArray[i]);
-
-            if (quote === undefined) {
-                data.push({"symbol": theArray[i], "status": "error", "data": {}});
+                return objInsert;
+            });
+        } catch (err) {
+            console.error("An error occured while fetching data: " + err);
+            
+            if (err.response) {
+                // Server responded with a status other than 2xx
+                console.error("Backend responded with error:", err.response);
+                setError(prevState => {
+                    const objError = {"id": theId, "error": err.response.data};
+                    return [...prevState, objError];
+                });
+            } else if (err.request) {
+                // Request was made, but no response received
+                console.error("No response from server: ", err.request);
+                setError(prevState => {
+                    const objError = {"id": theId, "error": "No response from the server. Please try again later"};
+                    return [...prevState, objError];
+                });
             } else {
-                data.push({"symbol": theArray[i], "status": "success", "data": getStockDataTemp(theArray[i])["Global Quote"]});
+                // Other errors
+                console.error("An unexpected error occurred: ", err.message);
+                setError(prevState => {
+                    const objError = {"id": theId, "error": err.message};
+                    return [...prevState, objError];
+                })
             }
         }
-        
-        setStockList(data);
+    }*/
+
+    // TEMPORARY GET STOCK DATA HELPER FUNCTION
+    const fetchStockData = (symb) => {
+        // random number generation for ID
+        const randomNum = getRandomNum();
+        const theId = symb + randomNum;
+
+        try {
+            let response = TemporaryServ(symb);
+            // functional update
+            setStockList(prevState => {
+                let objInsert = [...prevState,
+                    {
+                        "id": theId,
+                        "symbol": symb,
+                        "data": response["Global Quote"]
+                    }
+                ];
+
+                return objInsert;
+            });
+        } catch (err) {
+            console.error('Error occured fetching data', err);
+
+            setError(prevState => {
+                let objError = {"id": theId, "error": err};
+                return [...prevState, objError];
+            })
+        }
     }
 
-    useState(() => {
-        const testArray = ["IBM", "NVDA", "AAPL"];
+    // Load stock list on page load
+    useEffect(() => {
+        for (let i = 0; i < stockWatch.length; i++) {
+            fetchStockData(stockWatch[i]);
+        }
+    }, [])
 
-        buildListFromArray(testArray);
-    }, []);
-    // ***^^^Load user's existing list data^^^***
+
+    const deleteListing = (id) => {
+        setStockList(
+            stockList.filter(listing => listing.id !== id)
+        );
+    }
+
+    const deleteErrorMessage = (id) => {
+        setError(
+            error.filter(listing => listing.id !== id)
+        );
+    }
 
 
     return (
@@ -74,29 +143,73 @@ const Watchlist = () => {
             </div>
 
             {stockList.map(listing => (
-                <div key={listing["data"]["symbol"]} className={style.stockCard}>
-                    <Link to={'/stock/' + listing["symbol"]}>
-                        <div className={style.listGrid}>
+                <div key={listing["id"]} className={style.stockCard}>
+                    
+                        <div className={style.listFlexWrap}>
+                            <div className={style.listMainContent}>
+                                <Link to={'/stock/' + listing["symbol"]}>
+                                    <div className={style.listGrid}>
+
+                                            <div>
+                                                <span className={style.stockSymbol}>
+                                                    {listing["data"]["01. symbol"]}
+                                                </span>
+                                            </div>
+                                            <div>
+                                                <span>{listing["data"]["05. price"]}</span>
+                                                <br />
+                                                <span className={parseInt(listing["data"]['09. change']) >= 0 ? style.stockGreen : style.stockRed}>
+                                                    {parseInt(listing["data"]['09. change']) >= 0 ? '+' : ''}
+                                                    {listing["data"]['09. change']}
+                                                    &nbsp;({parseInt(listing["data"]['09. change']) >= 0 ? '+' : ''}{listing["data"]['10. change percent']})
+                                                </span>
+                                            </div>
+
+                                        
+                                    </div>
+                                </Link>
+                            </div>
 
                             <div>
-                                <span className={style.stockSymbol}>
-                                    {listing["data"]["01. symbol"]}
-                                </span>
+                                <button
+                                    className={style.btn1}
+                                    onClick={() => {deleteListing(listing["id"])}}>
+                                    Remove
+                                </button>
                             </div>
-                            <div>
-                                <span>{listing["data"]["05. price"]}</span>
-                                <br />
-                                <span className={parseInt(listing["data"]['09. change']) >= 0 ? style.stockGreen : style.stockRed}>
-                                    {parseInt(listing["data"]['09. change']) >= 0 ? '+' : ''}{listing["data"]['09. change']} ({parseInt(listing["data"]['09. change']) >= 0 ? '+' : ''}{listing["data"]['10. change percent']})
-                                </span>
-                            </div>
-
                         </div>
-                    </Link>
+                    
                 </div>
             ))}
 
-            {showAddMenu && <AddMenu visibleState={setShowAddMenu} getStockData={getStockDataTemp} setStockList={handleAddToList} />}
+            {error.map(listing => (
+                <div key={listing["id"]} className={style.stockCard}>
+                    <div className={style.listFlexWrap}>
+
+                        <div className={style.listMainContent}>
+                            <div className={style.listGrid}>
+                                <div>
+                                    <span className={style.stockError}>
+                                        Error - {listing.error.message}
+                                    </span>
+                                </div>
+                                
+                            </div>
+                        </div>
+
+                        <div>
+                            <button
+                                className={style.btn1}
+                                onClick={() => {deleteErrorMessage(listing["id"])}}>
+                                Dismiss
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+            ))}
+
+            {showAddMenu && <AddMenu visibleState={setShowAddMenu} fetchStockData={fetchStockData} />}
         </div>
     );
 }
@@ -143,17 +256,6 @@ const TemporaryServ = (symbol) => {
 }
 // ***END OF TEMPORARY FUNCTION***
 
-
-
-/**
- * 
- * @param {*} num Number to be adjusted
- * @param {*} precision number of decimal places
- * @returns Adjusted number
- */
-function setPrecision(num, precision) {
-    return num.toPrecision(precision);
-}
 
 
 /**
@@ -213,123 +315,17 @@ const AddEntry = ({symbol}) => {
 
 
 
-    
-}
 
-/*
-// Build the individual entry
-const EntryBuilder = ({symbol}) => {
-    const [stockData, setStockData] = useState(null);
-    const [error, setError] = useState(null);
-
-    const symbolData = {symbol}.symbol; // get just the symbol itself without encapsulating properties
-
-    useEffect(() => {
-        const getStockData = async () => {
-            try {
-                console.log("Fetching data...");
-                const response = await axios.get(`stock/${symbolData}`);
-                setStockData(response.data);
-            } catch (err) {
-                console.error("An error occured while fetching data: " + err);
-                
-                if (err.response) {
-                    // Server responded with a status other than 2xx
-                    console.error("Backend responded with error:", err.response);
-                    setError(`Error ${err.response.status}: ${err.response.data}`);
-                } else if (err.request) {
-                    // Request was made, but no response received
-                    console.error("No response from server: ", err.request);
-                    setError("No response from the server. Please try again later.");
-                } else {
-                    // Other errors
-                    console.error("An unexpected error occurred: ", err.message);
-                    setError("An error occurred: ", err.message);
-                }
-            }
-        }
-
-        // make the call to getStockData
-        //getStockData();
-    }, []); // NOTE THE BRACKETS - NECESSARY TO RUN ONLY ON FIRST RUN INSTEAD OF EVERY RENDER!
-
-    //FIXME change to actual request later
-    useState(() => {
-        try {
-            const response = TemporaryServ(symbolData);
-            setStockData(response);
-        } catch(err) {
-            console.error("An error occured: " + err);
-            setError(`${err}`);
-        }
-    }, []);
-    //END FIXME
-    
-    if (stockData === null) {
-        return (
-            <div className={style.stockCard}>
-                Loading...
-                {error && <div className={style.stockError}>{error}</div>}
-            </div>
-        );
-    }
-
-
-    // set equal to stock data itself so you don't have to type ['Global Quote'] every time
-    const stockQuote = stockData['Global Quote'];
-
-    // + or - based on change in stock price
-    const changeSign = parseInt(stockQuote['09. change']) >= 0 ? '+' : '';
-    const changeColor = parseInt(stockQuote['09. change']) >= 0 ? style.stockGreen : style.stockRed;
-
-    return (
-        <div className={style.stockCard}>
-                <Link to={'/stock/' + symbolData}>
-                    <div className={style.listGrid}>
-
-                        <div>
-                            <span className={style.stockSymbol}>
-                                {stockQuote["01. symbol"]}
-                            </span>
-                        </div>
-                        <div>
-                            <span>{stockQuote["05. price"]}</span>
-                            <br />
-                            <span className={changeColor}>
-                                {changeSign}{stockQuote['09. change']} ({changeSign}{stockQuote['10. change percent']})
-                            </span>
-                        </div>
-
-                    </div>
-                </Link>
-            </div>
-    );
 }
 
 
-const ListBuilder = () => {
-    // TEMPORARY
-    const listOfSymbols = ['IBM', 'NVDA', 'AAPL'];
-    let items = [];
-
-    for (let i = 0; i < listOfSymbols.length; i++) {
-        items.push(<EntryBuilder symbol={listOfSymbols[i]} key={i} />);
-    }
-
-    return (
-        items
-    );
-}
-*/
-
-const AddMenu = ({visibleState, getStockData, setStockList}) => {
+const AddMenu = ({visibleState, fetchStockData}) => {
     const [inputSym, setInputSym] = useState("");
     
     const addStock = () => {
         // insert code to add to user record
 
-        const data = getStockData(inputSym);
-        setStockList(inputSym, data["Global Quote"]);
+        fetchStockData(inputSym);
     }
 
     return (
