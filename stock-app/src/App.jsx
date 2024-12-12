497158a8596bd84ce127cf33e5dfaf07efd7156b
Anthony <149122963+antnay@users.noreply.github.com>
2024-12-11 18:43:24 -0800

    Revert "Anthony routes"

--- stock-app/src/App.jsx
+++ stock-app/src/App.jsx
@@ -1,36 +1,16 @@
+
 import { Routes, Route } from 'react-router-dom';
-import { ProtectedRoutes } from "./routes/ProtectedRoutes"
 import Home from './routes/Home';
-import About from './routes/About';
-import TermsAndConds from './routes/TermsAndConds';
-import Contact from './routes/Contact';
-import Register from './components/Forms/Register';
-import Login from './components/Forms/Login';
-import UserProfile from './routes/UserProfile';
-import Dashboard from  './routes/Dashboard';
 import Stock from './routes/Stock';
-import AllStocks from './routes/AllStocks';
-import StockGraph from './routes/StockGraph';
-import Search from './routes/Search';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/t&c" element={<TermsAndConds />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-      <Route element={<ProtectedRoutes />}> {/* Anything within this tag is protected */}
-        <Route path="/profile/:userId" element={<UserProfile />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/stocks" element={<AllStocks />} />
-        <Route path="/stock/:symbol/graph" element={<StockGraph />} />
-        <Route path="/stock/:symbol" element={<Stock />} />
-        <Route path="/search" element={<Search />} />
-      </Route>
-    </Routes>
+   <>
+   <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/stock/:symbol" element={<Stock />} />
+   </Routes>
+   </>
   );
 }
 
--- stock-app/src/routes/Home.jsx
+++ stock-app/src/routes/Home.jsx
@@ -1,92 +1,11 @@
-import React from 'react';
-//import SearchBar from '../components/SearchBar'
-import style from './style/home.module.css';
-import BigHeader from '../components/BigHeader';
-import GLPanel from '../components/GainerLoser';
-import NavBar from '../components/NavBar';
-import Footer from '../components/Footer';
-
-/**
- * This is the main home component, rendering the main layout.
- * Home contains sections: a navigation bar, a main header, a "Top Gainers and Losers" section, etc.
- * No direct interaction with the AlphaVantage API
- */
-
-const appName = "Universal Wealth";
+import React from 'react'
+import SearchBar from '../components/SearchBar'
 
 const Home = () => {
 
-    /**
-     * Hierarchy:
-     *  Nav bar + search and sign in
-     *  Main header
-     *  Top Gainers and Losers (Stocks to watch, not watchlist)
-     *  Market news
-     *  About us blerb
-     */
     return (
-        <>
-            <NavBar />
-            <MainHeader />
-            <NewsAndStats />
-            <AboutBlerb />
-            <Footer />
-        </>
+        <SearchBar />
     )
-}
-
-const MainHeader = () => {
-    const headText = appName;
-    const headSubtext = "SEE WHAT'S MOVING THE MARKET";
-    const bgUrl = "https://wallpapers.com/images/featured/stock-market-pd5zksxr07t7a4xu.jpg";
-    const bgGradient = "true";
-    const btnText = "GET STARTED";
-    const btnHref = "https://i.gifer.com/origin/7a/7a47d1014b0ac74e8db76bb7e2253e01_w200.gif";
-
-    return (
-        <BigHeader
-            headText={headText}
-            headSubtext={headSubtext}
-            bgUrl={bgUrl}
-            bgGradient={bgGradient}
-            btnText={btnText}
-            btnHref={btnHref}
-        />
-    );
-}
-
-const NewsAndStats = () => {
-    return (
-        <section className={style.newsStatsSectionWrap}>
-            <div className={style.newsStatsContainer}>
-
-                <div className={style.newsWrap}>
-                    <h2 className={style.head}>Market News</h2>
-                    
-                </div>
-
-                <div className={style.glWrap}>
-                    <h2 className={style.head}>Top Gain/Loss</h2>
-                    <GLPanel />
-                </div>
-
-            </div>
-        </section>
-    );
-}
-
-const AboutBlerb = () => {
-    return (
-        <section>
-            <h2 className="center">About Us</h2>
-            <p className={style.aboutParagraph}>
-                At Universal Wealth, we aim to connect users with the latest
-                stock market moves and news in a simple to use interface. This 
-                app formed as part of a project by the Tech Startup Club 
-                at the University of Washington Tacoma.
-            </p>
-        </section>
-    );
 }
 
 export default Home
